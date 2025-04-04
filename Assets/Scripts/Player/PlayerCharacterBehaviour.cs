a737a3c4085a13bd9a6d820215343be5e6b85548
Paulo Heitor Carrari <paulo.hc5@puccampinas.edu.br>
2025-04-04 19:38:43 -0300

    fixed problem with player reloading shotguns 2 times

--- Assets/Scripts/Player/PlayerCharacterBehaviour.cs
+++ Assets/Scripts/Player/PlayerCharacterBehaviour.cs
@@ -9,7 +9,7 @@ public class PlayerCharacterBehaviour : StateMachineBehaviour
     private Gun equippedGun;        
 
     private PlayerCombatStates FindCombatState(AnimatorStateInfo stateInfo)
-    {
+    {     
         if (stateInfo.IsTag("RaiseWeapon")) return PlayerCombatStates.RAISING;
         if (stateInfo.IsTag("Attack")) return PlayerCombatStates.ATTACKING;        
         if (stateInfo.IsTag("Fire")) return PlayerCombatStates.FIRING;        
@@ -50,7 +50,13 @@ public class PlayerCharacterBehaviour : StateMachineBehaviour
         }        
         
     }
-    
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerCharacter.PlayerCombatStates = FindCombatState(stateInfo);
+    }
+
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {        
