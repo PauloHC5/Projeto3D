3ae0b94d19afca8a16686479ebcaac74b256049a
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-05-18 19:23:58 -0300

    splited movement for mainCameraPos and armsHeadbob

--- Assets/Scripts/Player/CameraHeadbob.cs
+++ Assets/Scripts/Player/CameraHeadbob.cs
@@ -3,40 +3,39 @@ using UnityEngine;
 
 public class CameraHeadbob : MonoBehaviour
 {
+    [SerializeField] private Transform mainCameraPos;
+    [SerializeField] private Transform armsHeadbob;
+
     [Range(0.001f, 1f)]
     [SerializeField] private float Amount = 0.002f;
 
     [Range(1f, 30f)]
-    [SerializeField] private float Frequency = 10f;
+    [SerializeField] private float Frequency = 10f;                  
 
     [Range(10f, 100f)]
-    [SerializeField] private float Smooth = 10f;
-
-    [SerializeField] private Transform armsHeadbob;
+    [SerializeField] private float Smooth = 10f;    
 
     private PlayerCharacterMovementController playerCharacterMovementController;    
-    private Vector3 originalPosition;
-    private float originalFrequency;
+    private Vector3 originalPosition;    
 
     private void Awake()
     {
-        playerCharacterMovementController = GetComponentInParent<PlayerCharacterMovementController>();
-        originalFrequency = Frequency;
+        playerCharacterMovementController = GetComponentInParent<PlayerCharacterMovementController>();                
     }
 
     void Start()
     {
-        originalPosition = transform.localPosition;
+        if (mainCameraPos)
+            originalPosition = mainCameraPos.localPosition;
     }
 
     
     void Update()
-    {
+    {        
         CheckForHeadbobTrigger();
 
         if (playerCharacterMovementController.PlayerMovementStates == PlayerMovementStates.CROUCH || playerCharacterMovementController.PlayerMovementStates == PlayerMovementStates.CROUCHING)
             Frequency /= 2f;
-        else Frequency = originalFrequency;
     }    
 
     private void CheckForHeadbobTrigger()
@@ -54,19 +53,30 @@ public class CameraHeadbob : MonoBehaviour
 
     private void StartHeadbob()
     {
-        Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Lerp(pos.y, Mathf.Sin(Time.time * Frequency) * Amount * 1.4f, Smooth * Time.deltaTime);
-        pos.x += Mathf.Lerp(pos.x, Mathf.Cos(Time.time * Frequency / 2f) * Amount * 1.6f, Smooth * Time.deltaTime);
+        if (mainCameraPos)
+        {
+            Vector3 camPos = Vector3.zero;
+            camPos.y += Mathf.Lerp(camPos.y, Mathf.Sin(Time.time * Frequency) * Amount * 1.4f, Smooth * Time.deltaTime);
+            camPos.x += Mathf.Lerp(camPos.x, Mathf.Cos(Time.time * Frequency / 2f) * Amount * 1.6f, Smooth * Time.deltaTime);
+            mainCameraPos.localPosition = camPos;
+        }
 
-        transform.transform.localPosition = pos;      
-        if(armsHeadbob) armsHeadbob.localPosition = pos;
+        if (armsHeadbob)
+        {            
+            Vector3 armsPos = Vector3.zero;
+            armsPos.y += Mathf.Lerp(armsPos.y, Mathf.Sin(Time.time * Frequency) * Amount * 1.4f, Smooth * Time.deltaTime);
+            armsPos.x += Mathf.Lerp(armsPos.x, Mathf.Cos(Time.time * Frequency / 2f) * Amount * 1.6f, Smooth * Time.deltaTime);
+            armsHeadbob.localPosition = armsPos;
+        }
     }
 
     private void StopHeadbob()
     {
-        if (transform.localPosition == originalPosition) return;
-
-        transform.localPosition = Vector3.Slerp(transform.localPosition, originalPosition, 1f * Time.deltaTime);
+        if (mainCameraPos)
+        {
+            if (mainCameraPos.localPosition == originalPosition) return;
+            mainCameraPos.localPosition = Vector3.Slerp(mainCameraPos.localPosition, originalPosition, 1f * Time.deltaTime);
+        }
         if (armsHeadbob) armsHeadbob.localPosition = Vector3.Slerp(armsHeadbob.localPosition, originalPosition, 1f * Time.deltaTime);
     }
 }
