383c87d27021b6e1e164c5da90b6832dd446ec45
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-06-01 21:11:09 -0300

    altered carnivorous plants sfx

--- Assets/Scripts/Weapons/CarnivovrousPlant.cs
+++ Assets/Scripts/Weapons/CarnivovrousPlant.cs
@@ -42,7 +42,7 @@ public class CarnivovrousPlant : Weapon
             animator.SetTrigger("Attack");
             GameManager.Instance?.Hud?.Bite();
             if(GameManager.Instance.Hud.EnemyOnRange) StartCoroutine(AttackRoutine());
-            SoundManager.PlayShootSound(weaponType, 0.5f); // Play the attack sound
+            SoundManager.PlayShootSound(weaponType, 1.0f); // Play the attack sound
         }
         else
         {
