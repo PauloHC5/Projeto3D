f8e39c9c8ec020114d1b6a3d3da8d178180e2df5
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-06-17 16:09:07 -0300

    moved PlayerGunAmmoEditor to a own script in a new folder called Editor

--- Assets/Scripts/Player/PlayerGunAmmoInitializer.cs
+++ Assets/Scripts/Player/PlayerGunAmmoInitializer.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -45,34 +44,4 @@ public class PlayerGunAmmoInitializer : MonoBehaviour
     }
 #endif
 
-}
-
-[CustomEditor(typeof(PlayerGunAmmoInitializer))]
-public class PlayerGunAmmoEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-
-        SerializedProperty gunsAmmo = serializedObject.FindProperty("gunsAmmo");
-
-        for (int i = 0; i < gunsAmmo.arraySize; i++)
-        {
-            SerializedProperty element = gunsAmmo.GetArrayElementAtIndex(i);
-
-            EditorGUILayout.BeginVertical(GUI.skin.box);
-
-            // Show ammoType as read-only
-            EditorGUI.BeginDisabledGroup(true);
-            EditorGUILayout.PropertyField(element.FindPropertyRelative("ammoType"));
-            EditorGUI.EndDisabledGroup();
-
-            // Allow editing ammoAmount
-            EditorGUILayout.PropertyField(element.FindPropertyRelative("ammoAmount"));
-
-            EditorGUILayout.EndVertical();
-        }
-
-        serializedObject.ApplyModifiedProperties();
-    }
 }
