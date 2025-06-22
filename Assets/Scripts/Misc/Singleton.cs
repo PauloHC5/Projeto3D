11b2acde2434b4ef0f5df2f025215e109a1125ed
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-06-22 20:03:07 -0300

    added a new property to Singleton that allows user decide which type of Singleton will be persistent between the scenes

--- Assets/Scripts/Misc/Singleton.cs
+++ Assets/Scripts/Misc/Singleton.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    private static T _instance;    
+    private static T _instance;
+
+    [SerializeField] protected bool _shouldPersistBetweenScenes = true;
 
     protected static T Instance
     {
@@ -16,16 +18,14 @@ public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
 
             if (_instance == null)
             {
-                // Find any existing instances of the singleton in the scene
                 var foundInstances = FindObjectsByType<T>(FindObjectsSortMode.None);
                 _instance = foundInstances.Length > 0 ? foundInstances[0] : null;
-                
+
                 if (_instance == null)
                 {
                     Debug.LogError($"Singleton instance of {typeof(T).Name} not found in the scene. Please ensure there is a {typeof(T).Name} object.");
                 }
 
-                // If there are multiple instances, destroy them
                 if (foundInstances.Length > 1)
                 {
                     for (int i = 1; i < foundInstances.Length; i++)
@@ -34,8 +34,8 @@ public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
                     }
                 }
 
-                // Make this instance persist across scene loads
-                if (Application.isPlaying)
+                // Only persist if ShouldPersistBetweenScenes is true
+                if (Application.isPlaying && _instance != null && _instance._shouldPersistBetweenScenes)
                 {
                     DontDestroyOnLoad(_instance.gameObject);
                 }
@@ -52,30 +52,20 @@ public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
 
         if (_instance == null)
         {
-            _instance = (T)this;
-            if (Application.isPlaying)
+            _instance = (T)this;            
+            if (_shouldPersistBetweenScenes)
             {
                 DontDestroyOnLoad(gameObject);
             }
         }
         else if (_instance != this)
         {
-            Destroy(gameObject); // Prevent duplicates
-        }
-
-        /*
-         if(_instance != null && _instance != this)
-        {
-            // If an instance already exists, destroy this one
             Destroy(gameObject);
-            return;
         }
-        */
     }
 
     private void OnApplicationQuit()
     {
-        // Clean up the instance when the application quits
         if (_instance != null)
             _instance = null;
     }
