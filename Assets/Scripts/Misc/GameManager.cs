f7436011ad48b335b14a08cb4e40b54c51063f96
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-06-23 18:36:40 -0300

    removed unnecessary imports

--- Assets/Scripts/Misc/GameManager.cs
+++ Assets/Scripts/Misc/GameManager.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class GameManager : Singleton<GameManager>   
 {
