d192bda7f3bb503a5165b39ef31f80f2310b334b
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-06-23 18:27:49 -0300

    implemented skip cutscene funcionality

--- Assets/Scripts/Misc/CutsceneManager.cs
+++ Assets/Scripts/Misc/CutsceneManager.cs
@@ -21,21 +21,70 @@ public struct CutsceneVideoFile
 }
 
 public class CutsceneManager : Singleton<CutsceneManager>
-{    
+{
+    [SerializeField] private GameObject _canvasCutscene;
     [SerializeField] public List<CutsceneVideoFile> videosFileNames;
+    [Range(0.5f, 1.5f)]
+    [SerializeField] private float _skipFillSpeed = 1.0f; // seconds to fill
 
-    private GameObject _canvasCutscene;
-    private VideoPlayer _videoPlayer;
-    private RenderTexture _renderTexture;
+    [SerializeField] private VideoPlayer _videoPlayer;
+    [SerializeField] private RenderTexture _renderTexture;
+    [SerializeField] private Slider _skipCutsceneSlider;
 
-    private void Awake()
+    [Header("Skip Slider Hide Settings")]
+    [SerializeField] private float _skipSliderHideDelay = 3.0f; // seconds to wait before hiding slider
+
+    private float _skipSliderInactivityTimer = 0f;
+
+    private void Update()
     {
-        _canvasCutscene = GetComponentInChildren<Canvas>(true).gameObject;
-        _videoPlayer = GetComponentInChildren<VideoPlayer>(true);
-        _renderTexture = GetComponentInChildren<RawImage>(true).texture as RenderTexture;
+        if (_videoPlayer.isPlaying && !_skipCutsceneSlider.gameObject.activeSelf && Input.anyKeyDown)
+        {
+            _skipCutsceneSlider.gameObject.SetActive(true);
+            _skipSliderInactivityTimer = 0f; // Reset timer when slider is shown
+        }
 
-        if(_canvasCutscene.activeSelf) _canvasCutscene.SetActive(false);
+        if (_skipCutsceneSlider.gameObject.activeSelf && _canvasCutscene.activeSelf)
+        {
+            HandleSkipCutscene();
 
+            // Reset timer if any input is detected
+            if (Input.anyKey)
+            {
+                _skipSliderInactivityTimer = 0f;
+            }
+            else
+            {
+                _skipSliderInactivityTimer += Time.deltaTime;
+                if (_skipSliderInactivityTimer >= _skipSliderHideDelay)
+                {
+                    _skipCutsceneSlider.gameObject.SetActive(false);
+                    _skipSliderInactivityTimer = 0f;
+                }
+            }
+        }
+    }
+
+    private void HandleSkipCutscene()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Fill the slider over _skipFillSpeed seconds
+            _skipCutsceneSlider.value += (_skipCutsceneSlider.maxValue / _skipFillSpeed) * Time.deltaTime;
+
+            if (_skipCutsceneSlider.value >= _skipCutsceneSlider.maxValue)
+            {
+                _skipCutsceneSlider.value = _skipCutsceneSlider.maxValue;
+                // Skip the cutscene
+                _videoPlayer.Stop();
+                _canvasCutscene.SetActive(false);
+            }
+        }
+        else
+        {
+            // Reset if space is released
+            _skipCutsceneSlider.value = 0f;
+        }
     }
 
     public static IEnumerator StartCutscene(CutsceneType cutsceneType)
@@ -55,16 +104,17 @@ public class CutsceneManager : Singleton<CutsceneManager>
         RenderTexture.active = Instance._renderTexture;
         GL.Clear(true, true, Color.black);
         RenderTexture.active = activeRT;
-        
+
         Instance._canvasCutscene.SetActive(true);
+        Instance._skipCutsceneSlider.gameObject.SetActive(false);
+        Instance._skipSliderInactivityTimer = 0f; // Reset timer when cutscene starts
 
         Instance._videoPlayer.url = videoPath;
         Instance._videoPlayer.Play();
 
         // Wait until the video finishes playing
-        yield return new WaitUntil(() => !Instance._videoPlayer.isPlaying);     
-        
+        yield return new WaitUntil(() => !Instance._videoPlayer.isPlaying);
+
         Instance._canvasCutscene.SetActive(false);
     }
-    
 }
