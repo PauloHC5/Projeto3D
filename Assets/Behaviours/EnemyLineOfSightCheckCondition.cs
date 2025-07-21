3376d03119876a58d6779605054398814b43a44a
PauloHC5 <paulo.hc5@puccampinas.edu.br>
2025-07-21 20:02:47 -0300

    Add Chem Agent and Woodsman behaviour graphs
    Replaced the generic Behavior Graph with specific behaviour graphs for Chem Agent and Woodsman.

--- Assets/Behaviours/EnemyLineOfSightCheckCondition.cs
+++ Assets/Behaviours/EnemyLineOfSightCheckCondition.cs
@@ -1,17 +1,24 @@
 using System;
 using Unity.Behavior;
+using Unity.Properties;
 using UnityEngine;
 
 [Serializable, Unity.Properties.GeneratePropertyBag]
 [Condition(name: "Enemy line fo sight condition", story: "[Enemy] can see [Target] ?", category: "Conditions", id: "26e3f3a02ac4cbcc7a3fd2934e3e519a")]
 public partial class EnemyLineOfSightCheckCondition : Condition
 {
-    [SerializeReference] public BlackboardVariable<Enemy> Enemy;
-    [SerializeReference] public BlackboardVariable<GameObject> Target;    
-    
+    [SerializeReference] public BlackboardVariable<EnemyDetectionController> Enemy;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
 
     public override bool IsTrue()
     {
-        return Enemy.Value.PerformDetection(Target) != null;        
-    }   
+        if (Enemy.Value == null)
+        {
+            Debug.LogWarning("EnemyDetectionController component not found on the enemy. " +
+                             "Please ensure the enemy has an EnemyDetectionController component attached.");
+            return false;
+        }
+        
+        return Enemy.Value.PerformLineOfSightDetection(Target.Value);
+    }
 }
